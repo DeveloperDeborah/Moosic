45e87f09817fb5ef15d13f3115a2b1c51db60857
Kruithne <kruithne@gmail.com>
2013-06-07 02:46:59 +0100

    Moved to use LE buffering

--- src/no/runsafe/moosic/MusicTrack.java
+++ src/no/runsafe/moosic/MusicTrack.java
@@ -1,6 +1,7 @@
 package no.runsafe.moosic;
 
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,57 +10,70 @@ public class MusicTrack
 {
 	public MusicTrack(File songFile) throws Exception
 	{
+		// TODO: This seems overly complicated, make this better.
 		DataInputStream data = new DataInputStream(new BufferedInputStream(new FileInputStream(songFile)));
+		List<Byte> byteArray = new ArrayList<Byte>();
 
-		Plugin.output.write("byte: " + data.readByte());
-//		this.length = data.readShort(); // Song length
-		return;
-		/*Plugin.output.write("Length: " + this.length);
-		Plugin.output.write("Layers: " + data.readShort()); // Layers
-		this.songName = readString(data);
+		while (data.available() > 0)
+			byteArray.add(data.readByte());
+
+		byte[] bytes = new byte[byteArray.size()];
+
+		int index = 0;
+		for (byte nom : byteArray)
+		{
+			bytes[index] = nom;
+			index++;
+		}
+
+		ByteBuffer buffer = ByteBuffer.wrap(bytes);
+
+		this.length = buffer.getShort(); // Song length
+		buffer.getShort(); // Layers
+		this.songName = readString(buffer);
 
 		// We pull this data but have no need for it.
-		readString(data); // Song author
-		readString(data); // Original song author
-		readString(data); // Description
-		this.tempo = data.readShort() / 100;
-		data.readByte(); // Auto-save
-		data.readByte(); // Auto-save duration
-		data.readByte(); // Time sig
-		data.readInt(); // Minutes spent
-		data.readInt(); // Left clicks
-		data.readInt(); // Right clicks
-		data.readInt(); // Blocks added
-		data.readInt(); // Blocks removed
-		readString(data); // Midi/Schematic
+		readString(buffer); // Song author
+		readString(buffer); // Original song author
+		readString(buffer); // Description
+		this.tempo = buffer.getShort() / 100;
+		buffer.get(); // Auto-save
+		buffer.get(); // Auto-save duration
+		buffer.get(); // Time sig
+		buffer.getInt(); // Minutes spent
+		buffer.getInt(); // Left clicks
+		buffer.getInt(); // Right clicks
+		buffer.getInt(); // Blocks added
+		buffer.getInt(); // Blocks removed
+		readString(buffer); // Midi/Schematic
 
 		short tick = -1;
 		short jumps;
 
 		while (true)
 		{
-			jumps = data.readShort();
+			jumps = buffer.getShort();
 			if (jumps == 0)
 				break;
 
 			tick += jumps;
-			byte inst = data.readByte();
-			byte key = data.readByte();
+			byte inst = buffer.get();
+			byte key = buffer.get();
 
 			if (!this.notes.containsKey(tick))
 				this.notes.put(tick, new ArrayList<NoteBlockSound>());
 
 			this.notes.get(tick).add(new NoteBlockSound(new NoteBlockInstrument(inst), key));
-		}*/
+		}
 	}
 
-	private String readString(DataInputStream data) throws Exception
+	private String readString(ByteBuffer buffer) throws Exception
 	{
 		StringBuilder string = new StringBuilder();
-		int stringLength = data.readInt();
+		int stringLength = buffer.getInt();
 
 		for (int i = 0; i < stringLength; i++)
-			string.append((char) data.readByte());
+			string.append((char) buffer.get());
 
 		return string.toString();
 	}
