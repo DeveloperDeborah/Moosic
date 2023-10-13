004470e21fee980547cef7139bcb08a15f1240a8
DevDeborah <softwaredeveloperdeborah@gmail.com>
2023-10-13 15:09:16 -0400

    fix setting note block pitch

--- src/no/runsafe/moosic/NoteBlockSound.java
+++ src/no/runsafe/moosic/NoteBlockSound.java
@@ -15,7 +15,9 @@ public class NoteBlockSound
 	public void playAtLocation(ILocation location, float volume)
 	{
 		float pitch = (float) Math.pow(2.0, ((double) (key - 33) - 12.0) / 12.0);
-		location.getWorld().playSound(location, this.instrument.getSound(), (volume * this.volume) / 100, this.pitch);
+		if (this.pitch != 0)
+			pitch *= this.pitch;
+		location.getWorld().playSound(location, this.instrument.getSound(), (volume * this.volume) / 100, pitch);
 	}
 
 	private final NoteBlockInstrument instrument;
